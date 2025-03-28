24409079fc5276daaafe39bc35953cb313e9da70
joeycloudio <joeyacostax@gmail.com>
2025-03-28 15:04:45 -0700

    Update creating a Kubernetes cluster.sh

--- Kubernetes/creating a Kubernetes cluster.sh
+++ Kubernetes/creating a Kubernetes cluster.sh
@@ -1,6 +1,6 @@
 eksctl create cluster \
 --name [cluster name] \ # update with cluster name
---nodegroup-name [node group name] \ # update with node group name
+--nodegroup-name [node group name] \ # update with node group name (group of EC2 instances that will run your containers)
 --node-type t2.micro \
 --nodes 3 \ #Start your node group with 3 nodes and automatically scale between 1 (minimum) and 3 nodes (maximum) based on demand.
 --nodes-min 1 \
@@ -9,3 +9,4 @@ eksctl create cluster \
 --region [region] #update with region
 
 # EKS charges $0.10 for every hour your cluster is running.
+# node group is a group of EC2 instances that will run your containers.
