766c056c27f85b91c981a99005f3007c656622fe
joeycloudio <joeyacostax@gmail.com>
2025-03-29 11:02:34 -0700

    Update download and install eksctl.sh

--- Kubernetes/download and install eksctl.sh
+++ Kubernetes/download and install eksctl.sh
@@ -2,6 +2,7 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 sudo mv -v /tmp/eksctl /usr/local/bin
 
 ---------------------------------------------
+# run this in your terminal from the EC2 instance you're installing eksctl on
 # downloads latest eksctl from GitHub
 # second command moves it to a directory that lets you run eksctl from anywhere in your terminal
 
