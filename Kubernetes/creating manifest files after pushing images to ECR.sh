3ac9c74956d1b0839c25fe9f2aad6ffffe104004
joeycloudio <joeyacostax@gmail.com>
2025-04-03 16:25:32 -0700

    Update creating manifest files after pushing images to ECR.sh

--- Kubernetes/creating manifest files after pushing images to ECR.sh
+++ Kubernetes/creating manifest files after pushing images to ECR.sh
@@ -23,7 +23,9 @@ spec:
     spec:
       containers:
         - name: nextwork-flask-backend
-          image: YOUR-ECR-IMAGE-URI-HERE
+          image: YOUR-ECR-IMAGE-URI-HERE #You should replace YOUR-ECR-IMAGE-URL-HERE with the URI of the Docker image you pushed to Amazon ECR. This lets Kubernetes know where to pull the container image from when it deploys your application.
           ports:
             - containerPort: 8080
 EOF
+
+nano flask-deployment.yaml
