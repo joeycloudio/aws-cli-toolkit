ef03a5940961481a3464cc369f6c34a11771d1ef
joeycloudio <joeyacostax@gmail.com>
2025-04-03 16:22:54 -0700

    Update creating manifest files after pushing images to ECR.sh

--- Kubernetes/creating manifest files after pushing images to ECR.sh
+++ Kubernetes/creating manifest files after pushing images to ECR.sh
@@ -1,3 +1,29 @@
 cd ..
 mkdir manifests
 cd manifests
+
+# making a directory then making a deployment manifest below
+
+cat << EOF > flask-deployment.yaml
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nextwork-flask-backend
+  namespace: default
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: nextwork-flask-backend
+  template:
+    metadata:
+      labels:
+        app: nextwork-flask-backend
+    spec:
+      containers:
+        - name: nextwork-flask-backend
+          image: YOUR-ECR-IMAGE-URI-HERE
+          ports:
+            - containerPort: 8080
+EOF
