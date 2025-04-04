03206a2adf724dec9668218fd179b4dee50b30e5
joeycloudio <joeyacostax@gmail.com>
2025-04-04 13:13:45 -0700

    Update EKS-Terraform-Workflow.sh

--- EKS-Terraform-Workshop/EKS-Terraform-Workflow.sh
+++ EKS-Terraform-Workshop/EKS-Terraform-Workflow.sh
@@ -11,6 +11,8 @@ mv tfekscode environment/
 
 source ./bootstrap.sh
 # setting up a workshop tool above - "helper script"
+# likely installed terraform here and CLI tools, jq, kubectl, terraform, etc.
+# set ups environment variables
 
 Ctrl + C
 # needed to stop command due to aws configure not set
@@ -40,3 +42,13 @@ aws ec2 describe-instances --region eu-west-2
 aws ec2 describe-vpcs --filters Name=isDefault,Values=true --region eu-west-2
 aws ec2 describe-internet-gateways --region eu-west-2
 #checking lab default VPC was actually created since we are adpating this
+
+INITIAL TERRAFORM SETUP TO CREATE THE TF STATE BUCKET (PART 1 OF 8)
+cd ~/environment/tfekscode/tf-setup
+terraform init
+terraform validate # should say success! the configuration is valid
+terraform plan -out tfplan # plan the deployment
+
+export AWS_REGION=eu-west-2
+export TF_VAR_region=eu-west-2
+# had to set environment variables for terraform because it was going to create resources in the wrong region
