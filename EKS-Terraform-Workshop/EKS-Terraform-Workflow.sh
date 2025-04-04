4736dc4cabec58dc03693dc2d5b09f9357744e10
joeycloudio <joeyacostax@gmail.com>
2025-04-04 11:11:34 -0700

    Update EKS-Terraform-Workflow.sh

--- EKS-Terraform-Workshop/EKS-Terraform-Workflow.sh
+++ EKS-Terraform-Workshop/EKS-Terraform-Workflow.sh
@@ -1,12 +1,14 @@
 rm -rf tfekscode
 git clone -b module https://github.com/aws-samples/terraform-eks-code.git tfekscode
-
 # clone workshop repo
 
 mv tfekscode environment/
-
 # move file / reorganize them
 
 source ./bootstrap.sh
-
 # setting up a workshop tool above - "helper script"
+
+Ctrl + C
+# needed to stop command due to aws configure not set
+
+aws configure
