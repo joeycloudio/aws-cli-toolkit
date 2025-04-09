03c34495814dc2b5d6332faeabc699c28d942455
joeycloudio <joeyacostax@gmail.com>
2025-04-09 15:13:20 -0700

    Create instance metadata service.sh

---
+++ automating aws services with scripting & aws cli/instance metadata service.sh
@@ -0,0 +1,11 @@
+./show-credentials
+
+# data about your instance that you can use to config and manage the running instance
+# a set of security credentials is included in the data and used for all commands in lab
+
+# worked as follows
+# role named scripts was created with appropriate permissions to run the lab
+# EC2 instance that was used was launched with the scripts role
+# AWS CLI and Python SDK automatically retrieved the security credentials via Instance Metadata Service
+
+# this is an example of role-based access (ec2 with an attached role) vs key-based access (locally)
