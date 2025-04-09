919eb801369ed64ea63dc9490233aca54b81877d
joeycloudio <joeyacostax@gmail.com>
2025-04-09 15:22:45 -0700

    Update instance metadata service.sh

--- automating aws services with scripting & aws cli/instance metadata service.sh
+++ automating aws services with scripting & aws cli/instance metadata service.sh
@@ -10,3 +10,7 @@
 
 # this is an example of role-based access (ec2 with an attached role) vs key-based access (locally)
 # this is only availabe with EC2
+
+# Manually fetches temporary IAM credentials from the EC2 Instance Metadata Service (IMDSv2). 
+# Shows what the instance is using behind the scenes for AWS CLI/Boto3 access via its attached IAM role. 
+# Used mainly for debugging or visibility.
