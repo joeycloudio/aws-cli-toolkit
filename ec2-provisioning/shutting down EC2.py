a2cd235d5ec5a80eb4a670d25cc5b3937972482f
joeycloudio <joeyacostax@gmail.com>
2025-04-09 14:07:09 -0700

    Update shutting down EC2.py

--- ec2-provisioning/shutting down EC2.py
+++ ec2-provisioning/shutting down EC2.py
@@ -27,3 +27,14 @@ for instance in ec2.instances.all():
       elif action == 'terminate' and state != 'terminated':
         print ("Terminating instance", instance.id)
         instance.terminate()
+
+# add tag with key "stopinator" and value "stop"
+# then modify tag with value "terminate"
+
+additional ideas
+----------------
+# schedule to stop instances every night
+# mark instances you want to keep running, then have stopinator stop only unknown instances (but don't terminate since they may be important)
+# create another script that turns them on in the morning
+# set different actions for weekdays and weekends
+# use another tag to identify how many hours you want instance to run (for experiments), schedule stopinator to run hourly and configure to terminate instances that run longer than the indicated number of hours
