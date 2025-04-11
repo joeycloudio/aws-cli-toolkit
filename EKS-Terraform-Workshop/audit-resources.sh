1797bc36ac2e5f2d280a97af6538684862dc65be
joeycloudio <joeyacostax@gmail.com>
2025-04-11 12:01:12 -0700

    Update audit-resources.sh

--- EKS-Terraform-Workshop/audit-resources.sh
+++ EKS-Terraform-Workshop/audit-resources.sh
@@ -1,7 +1,22 @@
 REGION="eu-west-2"; echo "== EC2 INSTANCES ==" && aws ec2 describe-instances --region $REGION --query 'Reservations[*].Instances[*].{ID:InstanceId,State:State.Name,Type:InstanceType,AZ:Placement.AvailabilityZone}' --output table && echo "== EBS VOLUMES ==" && aws ec2 describe-volumes --region $REGION --query 'Volumes[?State==`in-use`].[VolumeId,Size,AvailabilityZone]' --output table && echo "== ELBs ==" && aws elb describe-load-balancers --region $REGION --output table && aws elbv2 describe-load-balancers --region $REGION --output table && echo "== S3 BUCKETS ==" && aws s3api list-buckets --query 'Buckets[*].Name' --output table && echo "== EKS CLUSTERS ==" && aws eks list-clusters --region $REGION --output table && echo "== RDS INSTANCES ==" && aws rds describe-db-instances --region $REGION --query 'DBInstances[*].{ID:DBInstanceIdentifier,Status:DBInstanceStatus,Class:DBInstanceClass}' --output table && echo "== LAMBDAS ==" && aws lambda list-functions --region $REGION --query 'Functions[*].{Function:FunctionName,Runtime:Runtime}' --output table && echo "== CLOUDFORMATION STACKS ==" && aws cloudformation list-stacks --region $REGION --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE --query 'StackSummaries[*].{Name:StackName,Status:StackStatus}' --output table
 
+# for Linux
 aws ce get-cost-and-usage \
   --time-period Start=$(date -d "-3 days" +%Y-%m-%d),End=$(date +%Y-%m-%d) \
   --granularity DAILY \
   --metrics "UnblendedCost" \
+  --output table
+
+  # for macOS
+  aws ce get-cost-and-usage \
+  --time-period "{\"Start\":\"$(date -v -3d +%Y-%m-%d)\",\"End\":\"$(date +%Y-%m-%d)\"}" \
+  --granularity DAILY \
+  --metrics "UnblendedCost" \
+  --output table
+
+aws ce get-cost-and-usage \
+  --time-period "{\"Start\":\"$(date -v -3d +%Y-%m-%d)\",\"End\":\"$(date +%Y-%m-%d)\"}" \
+  --granularity DAILY \
+  --metrics "UnblendedCost" \
+  --group-by Type=DIMENSION,Key=SERVICE \
   --output table
